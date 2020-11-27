fd467f82090d2ce2c3de7f99e5e3c1587b6194a6
Rafael Paiva <rafa_klose@hotmail.com>
2020-11-27 05:41:41 -0300

    install of dependencies and raw files built

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./screens/Home";
+import Detalhes from "./screens/Detalhes";
+const { Navigator, Screen } = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Screen name="Home" component={Home} options={{ headerShown: false }} />
+        <Screen
+          name="Detalhes"
+          component={Detalhes}
+          options={{ headerShown: false }}
+        />
+      </Navigator>
+    </NavigationContainer>
+  );
+}
